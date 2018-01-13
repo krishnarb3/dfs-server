cb9ec8886a856d5c02126f9686312529830dd0c7
Rb Krishna <krishnarb3@gmail.com>
2018-01-13 18:11:55 +0530

    Initial configuration using ignite

--- web/src/main/kotlin/edu/nitt/distributed/MainVerticle.kt
+++ web/src/main/kotlin/edu/nitt/distributed/MainVerticle.kt
@@ -1,13 +1,21 @@
 package edu.nitt.distributed
 
+import io.vertx.kotlin.core.VertxOptions
 import io.vertx.reactivex.core.AbstractVerticle
+import io.vertx.reactivex.core.Vertx
 import io.vertx.reactivex.ext.web.Router
 
+import io.vertx.spi.cluster.ignite.IgniteClusterManager
 
 class MainVerticle: AbstractVerticle() {
     override fun start() {
-        val router: Router = Router.router(vertx)
-        router.route("/eventbus/*").handler(eventBusHandler(vertx))
-        vertx.createHttpServer().requestHandler({ router.accept(it) }).listen(11123)
+        val clusterManager = IgniteClusterManager()
+        val options = VertxOptions().setClusterManager(clusterManager)
+        Vertx.rxClusteredVertx(options)
+            .subscribe({ vertx ->
+                val router: Router = Router.router(vertx)
+                router.route("/eventbus/*").handler(eventBusHandler(vertx))
+                vertx.createHttpServer().requestHandler({ router.accept(it) }).listen(11123)
+            })
     }
 }
